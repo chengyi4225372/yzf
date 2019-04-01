34053c2ec35940309350c66af2c3727cc3b3cc0c
chengyi <732345907@qq.com>
2019-04-01 16:41:51 +0800

    新闻完成

--- application/index/controller/News.php
+++ application/index/controller/News.php
@@ -16,8 +16,25 @@ class News extends Common {
 
     //新闻首页
     public function index() {
+         //热门
+        $remen = Db::name('new')->where('tuijian',1)->select();
+        //普通
+        $nomal = Db::name('new')->where('tuijian',0)->paginate(15);
+        $this->assign('remen',$remen);
+        $this->assign('nomal',$nomal);
+
         return $this->view->fetch();
     }
 
+    //新闻详情
+    public function detail(){
+        $id =input('get.id','','intval');
+        if($id <=0){
+            $this->error('参数错误','index/news/index');
+        }
+        $res = Db::name('new')->where('id',$id)->find();
+        $this->assign('res',$res);
+        return  $this->fetch();
+    }
 
 }
