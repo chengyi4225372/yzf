d6a9349345bbf074eb4ca921197fca0fef7d9fff
chengyi <732345907@qq.com>
2019-04-19 15:23:18 +0800

    完成首页户型

--- application/api/controller/Send.php
+++ application/api/controller/Send.php
@@ -10,14 +10,28 @@ namespace app\api\controller;
 use think\Controller;
 use service\FileService;
 use think\Db;
-
-
+use think\Request;
 class  Send extends Controller
 {
    //预约设计
     public function sheji(){
         $data = input('post.');
-        dump($data);
-        exit();
+        $res = Db::name('yuyue')->insert(
+            array(
+            'channel'=>$data['channel'],
+            'names'=>$data['names'],
+            'phone'=>$data['phone'],
+            'xiao'=>$data['xiao'],
+            'square'=>$data['square'],
+             'shi'=>$data['shi'],
+             'ting'=>$data['ting'],
+             'wei'=>$data['wei'],
+            )
+        );
+        if($res){
+          $this->result('','200','提交成功！','json');
+        }else{
+            $this->result('','400','提交失败！','json');
+        }
     }
 }
