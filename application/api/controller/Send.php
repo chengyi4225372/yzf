d3124318f0dd8d5ac869424df9c38a710e44ee66
chengyi <732345907@qq.com>
2019-04-19 15:50:17 +0800

    完成预约 以及删除部分页面js拆件

--- application/api/controller/Send.php
+++ application/api/controller/Send.php
@@ -13,7 +13,7 @@ use think\Db;
 use think\Request;
 class  Send extends Controller
 {
-   //预约设计
+   //我的户型
     public function sheji(){
         $data = input('post.');
         $res = Db::name('yuyue')->insert(
@@ -23,9 +23,9 @@ class  Send extends Controller
             'phone'=>$data['phone'],
             'xiao'=>$data['xiao'],
             'square'=>$data['square'],
-             'shi'=>$data['shi'],
-             'ting'=>$data['ting'],
-             'wei'=>$data['wei'],
+            'shi'=>isset($data['shi'])?$data['shi']:'',
+            'ting'=>isset($data['ting'])?$data['ting']:'',
+            'wei'=>isset($data['wei'])?$data['wei']:'',
             )
         );
         if($res){
@@ -34,4 +34,29 @@ class  Send extends Controller
             $this->result('','400','提交失败！','json');
         }
     }
+
+    //预约设计
+    public function yu(){
+        $data = input('post.');
+        $res = Db::name('yuyue')->insert(
+            array(
+                'channel'=>$data['channel'],
+                'names'=>$data['names'],
+                'phone'=>$data['phone'],
+                'xiao'=>$data['xiao'],
+                'square'=>$data['square'],
+                'shi'=>isset($data['shi'])?$data['shi']:'',
+                'ting'=>isset($data['ting'])?$data['ting']:'',
+                'wei'=>isset($data['wei'])?$data['wei']:'',
+            )
+        );
+        if($res){
+            $this->result('','200','提交成功！','json');
+        }else{
+            $this->result('','400','提交失败！','json');
+        }
+
+    }
+
+
 }
