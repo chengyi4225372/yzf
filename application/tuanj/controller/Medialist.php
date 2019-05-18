cb40d9dcfe110ad7ff36e6dbbb10dd65b2e18744
chengyi <732345907@qq.com>
2019-05-18 11:10:19 +0800

    ask 后台

---
+++ application/tuanj/controller/Medialist.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/18
+ * Time: 10:51
+ */
+namespace app\tuanj\controller;
+
+use think\Db;
+use controller\BasicAdmin;
+use service\DataService;
+
+class Medialist extends BasicAdmin {
+
+    private $dataform = 'media_list';
+    public $table ='media_cate';
+
+    public function index() {
+        $this->title = '装修自媒体列表';
+        list($get, $db) = [$this->request->get(), Db::name($this->dataform)];
+        (isset($get['keywords']) && $get['keywords'] !== '') && $db->whereLike('title|url', "%{$get['keywords']}%");
+        if (isset($get['date']) && $get['date'] !== '') {
+            list($start, $end) = explode(' - ', $get['date']);
+            $db->whereBetween('create_at', ["{$start} 00:00:00", "{$end} 23:59:59"]);
+        }
+        return parent::_list($db->order('id desc'));
+    }
+
+
+    protected function _data_filter(&$data) {
+        foreach ($data as $key => $val) {
+            $data[$key]['cates'] = Db::name($this->table)->where('id', '=', $val['mid'])->value('title');
+        }
+    }
+
+
+    /**
+     * 添加
+     * @return type
+     */
+    public function add() {
+        $cates =Db::name($this->table)->field('id,title')->select();
+        $this->assign('cates',$cates);
+        return $this->_form($this->dataform, 'form');
+    }
+
+    /**
+     * 编辑
+     * @return type
+     */
+    public function edit() {
+        $cates =Db::name($this->table)->field('id,title')->select();
+        $this->assign('cates',$cates);
+        return $this->_form($this->dataform, 'form');
+    }
+
+    /**
+     * 添加成功回跳处理
+     * @param bool $result
+     */
+    protected function _form_result($result) {
+        if ($result !== false) {
+            list($base, $spm, $url) = [url('@admin'), $this->request->get('spm'), url('tuanj/medialist/index')];
+            $this->success('数据保存成功！', "{$base}#{$url}?spm={$spm}");
+        }
+    }
+
+    /**
+     * 删除
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function del() {
+        if (DataService::update($this->dataform)) {
+            $this->success("删除成功!", '');
+        }
+        $this->error("删除失败, 请稍候再试!");
+    }
+
+    /**
+     * 禁用
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function forbid() {
+        if (DataService::update($this->dataform)) {
+            $this->success("禁用成功!", '');
+        }
+        $this->error("禁用失败, 请稍候再试!");
+    }
+
+    /**
+     * 禁用
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function resume() {
+        if (DataService::update($this->dataform)) {
+            $this->success("启用成功!", '');
+        }
+        $this->error("启用失败, 请稍候再试!");
+    }
+
+}
