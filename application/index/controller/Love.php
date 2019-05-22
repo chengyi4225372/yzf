478543adec8f87ec394761a455beb97447fc21be
chengyi <732345907@qq.com>
2019-05-22 15:08:22 +0800

    前端 爱家 装修事 完成

--- application/index/controller/Love.php
+++ application/index/controller/Love.php
@@ -13,7 +13,30 @@ use app\index\controller\Common;
 
 class Love extends Common
 {
-   public function index(){
-       return $this->fetch();
-   }
+
+    public $table ='love_list';
+
+    public function index(){
+
+        $count = Db::name($this->table)->count();
+        $list = Db::name($this->table)->field('id,title,img,desc,time')->order('id desc')->paginate(10);
+        $re = $this->remen();
+        $know =$this->know();
+        $this->assign('list',$list);
+        $this->assign('count',$count);
+        $this->assign('re',$re);
+        $this->assign('know',$know);
+        return $this->fetch();
+    }
+
+    public function detail(){
+        $id= input('get.id');
+        $res= Db::name($this->table)->where('id',$id)->find();
+        $re = $this->remen();
+        $know =$this->know();
+        $this->assign('res',$res);
+        $this->assign('re',$re);
+        $this->assign('know',$know);
+        return $this->fetch();
+    }
 }
